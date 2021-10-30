606e2caeec9a482aade3f7156e1a4f274ca13f6e
szabodev <64702997+szabodev@users.noreply.github.com>
2021-10-30 15:07:46 +0200

    Added files from my laptop
    Added files from my laptop using the web page.

---
+++ GitFromLaptop/src/main/java/com/example/demo/GitFromLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFromLaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFromLaptopApplication.class, args);
+	}
+
+}
